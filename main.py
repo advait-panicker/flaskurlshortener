ed6b4f4d7251ba0868591a68662ac7399228f6db
advait-panicker <advait.panicker@yahoo.com>
2024-08-18 11:33:45 -0400

    MongoDB setup

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 import json
 from random import choice
 from string import ascii_uppercase, ascii_lowercase, digits
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
@@ -10,8 +12,13 @@ url_chars = ascii_uppercase + ascii_lowercase + digits
 def random_string(n):
     return ''.join([choice(url_chars) for _ in range(n)])
 
-with open('redirects.json', 'r') as f:
-    links = json.loads(f.read())
+# load mongoDB connection string from environment variable
+connection_string = os.getenv('MONGO_CONNECTION_STRING', 'mongodb://localhost:27017/')
+client = MongoClient()
+db = client['url_shortener']
+collection = db['links']
+
+collection.create_index([("shortened_link", 1)], unique=True)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -19,17 +26,16 @@ def index():
         return render_template('index.html', new_link="", shown=False)
     else:
         new_link = random_string(6)
-        while links.get(new_link):
+        while collection.find_one({'shortened_link': new_link}):
             new_link = random_string(6)
-        links[new_link] = request.form['url']
-        with open('redirects.json', 'w') as f:
-            f.write(json.dumps(links))
+        collection.insert_one({'shortened_link': new_link, 'original_url': request.form['url']})
         return render_template('index.html', new_link=new_link, shown=True)
 
 @app.route('/<link>', methods=["GET"])
 def find(link):
-    if links.get(link):
-        return redirect(links[link])
+    query = collection.find_one({'shortened_link': link})
+    if query:
+        return redirect(query['original_url'])
     else:
         return render_template('error.html')
 
